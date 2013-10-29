ad824a38fc869b6dfe8883830abde1c6f0c0ab38
Bruno Pagno <brunopagno@gmail.com>
2013-10-28 22:12:58 -0200

    Organizing better layout of dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @last_few = current_user.transactions.limit(10).group_by { |t| t.date }
   end
 
   def monthly
