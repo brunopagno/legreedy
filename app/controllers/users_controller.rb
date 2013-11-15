1589c1580953147da29bfe8053ee4903eec430b1
Bruno Pagno <brunopagno@gmail.com>
2013-11-14 23:11:06 -0200

    Partially made 'graphics' work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     year = params[:year].present? ? params[:year] : Date.today.year
     month = params[:month].present? ? params[:month] : Date.today.month
     @date = Date.new(year.to_i, month.to_i)
-    @transactions = current_user.on(params[:year], params[:month]).order('date DESC')
+    @transactions = current_user.on(year, month).order('date DESC')
     @grouped_transactions = @transactions.group_by { |t| t.date }
     @stats = {}
     @stats[:total_in] = @transactions.select{ |t| t.way == TransactionWay::IN }.sum(&:value)
